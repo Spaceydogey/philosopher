eaacedb9d369e9bddf256c558264e5e0ed821ed9
Hugo Delmas <hdelmas@mi-c2-p5.s19.be>
2023-02-06 11:16:58 +0100

    euuh

--- philo/src/print_log.c
+++ philo/src/print_log.c
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 11:07:13 by hdelmas           #+#    #+#             */
-/*   Updated: 2023/01/29 13:27:19 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/02/06 11:07:46 by hdelmas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int	print_log(char *msg, t_data *philo)
 {
 	size_t	dt;
 
-	if (pthread_mutex_lock(philo->mutex.death_check) != 0)
-		return (1);
+	pthread_mutex_lock(philo->mutex.death_check);
 	if (philo->args->status == DEAD)
 	{
 		pthread_mutex_unlock(philo->mutex.death_check);
@@ -25,8 +24,7 @@ int	print_log(char *msg, t_data *philo)
 	}
 	pthread_mutex_unlock(philo->mutex.death_check);
 	dt = get_dtime(philo->args->start_time);
-	if (pthread_mutex_lock(philo->mutex.print) != 0)
-		return (1);
+	pthread_mutex_lock(philo->mutex.print);
 	printf("%lu\t%d\t%s\n", dt, philo->id, msg);
 	pthread_mutex_unlock(philo->mutex.print);
 	return (0);
